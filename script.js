412b77f54636b2297f313b48b88fd3eec6d60d88
Michael <107354194+MichaelIvory@users.noreply.github.com>
2025-06-30 14:02:19 +0700

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById('contactForm').addEventListener('submit', function(e) {
   e.preventDefault();
   alert('Thank you for your message!');
-  // Di sini kamu bisa menambahkan logika untuk mengirim data ke backend/email
 });
